230d00b46cbdd752c5e8328e09c87c20b6c62e6e
Dimas Pangestu <dimaspangestu19@gmail.com>
2021-06-08 17:28:48 +0700

    method vue

--- 1_awal/app.js
+++ 1_awal/app.js
@@ -1,8 +1,9 @@
-const app = new Vue({
+var app = new Vue({
     el: '#app',
     data: {
         maximum: 50,
-        products: null
+        products: null,
+        cart: []
     },
     mounted: function () {
         fetch('https://hplussport.com/api/products/order/price')
@@ -10,5 +11,10 @@ const app = new Vue({
             .then(data => {
                 this.products = data;
             });
+    },
+    methods: {
+        addItem: function (product) {
+            this.cart.push(product);
+        }
     }
 });
