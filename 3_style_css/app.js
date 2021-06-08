7f363c141ddc8883f30d3e8d9fc2aa0ab95e8c3e
Dimas Pangestu <dimaspangestu19@gmail.com>
2021-06-08 18:42:33 +0700

    Binding Attribute dengan Array dan Object

---
+++ 3_style_css/app.js
@@ -0,0 +1,23 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        maximum: 50,
+        products: null,
+        cart: [],
+        style: {
+            label: ['font-weight-bold', 'mr-2']
+        }
+    },
+    mounted: function () {
+        fetch('https://hplussport.com/api/products/order/price')
+            .then(response => response.json())
+            .then(data => {
+                this.products = data;
+            });
+    },
+    methods: {
+        addItem: function (product) {
+            this.cart.push(product);
+        }
+    }
+});
