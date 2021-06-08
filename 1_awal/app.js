e2f19bfeca6eeeff105f9a723fcc967ee32de266
Dimas Pangestu <dimaspangestu19@gmail.com>
2021-06-08 15:08:11 +0700

    Initial commit

---
+++ 1_awal/app.js
@@ -0,0 +1,11 @@
+const product = {
+    "name": "Bamboo Thermal Ski Coat",
+    "description": "ini deskripsi",
+    "price": "99",
+    "image": "https://hplussport.com/wp-content/uploads/2015/12/h-horizontal-logo.png"
+}
+
+const app = new Vue({
+    el: '#app',
+    data: product
+});
