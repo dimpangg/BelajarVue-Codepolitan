20f0775477369e159af10f17e96058257a82bddc
Dimas Pangestu <dimaspangestu19@gmail.com>
2021-06-10 23:21:48 +0700

    Membuat Component Price

--- 6_vue_cli/vue-cli/src/App.vue
+++ 6_vue_cli/vue-cli/src/App.vue
@@ -3,16 +3,19 @@
     <h1>IDShop</h1>
     <p class="animated fadeInLeft">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt ab dolorum minima doloremque perspiciatis culpa officia eum sit officiis asperiores! Ab facilis aspernatur minima enim corporis assumenda, quod nisi eaque.</p>
     <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
+    <price :value="4.23"></price>
   </div>
 </template>
 
 <script>
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import Price from "./components/Price.vue"
 
 export default {
   name: "App",
   components: {
     FontAwesomeIcon,
+    Price
   },
 };
 </script>
